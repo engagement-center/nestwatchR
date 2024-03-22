7c043bb461bc1687ce5fc0df08b79276b26b08b8
kimsavides <kim.savides@gmail.com>
2024-03-22 12:16:41 -0400

    quickstart data added

---
+++ R/wren-quickstart.R
@@ -0,0 +1,6 @@
+#' @title Data for NestWatch Workflow Examples
+#'
+#' @description Data from the NestWatch database (V2), subset to include all nesting data for Carolina ("carwre") and Bewick's ("bewwre") wrens. These are un-cleaned and un-filtered from the database.
+#'
+#' @format A dataframe with 40290 observations of 54 variables. See <https://doi.org/10.1002/ecy.4230> for details of variables.
+"wren_quickstart"
