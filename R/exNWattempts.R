29aba80286f2c292018a3a225dc5ddd595bed197
kimsavides <kim.savides@gmail.com>
2024-04-25 10:34:48 -0400

    typo

--- R/exNWattempts.R
+++ R/exNWattempts.R
@@ -1,6 +1,6 @@
 #' @title Sample NestWatch Checks Dataframe for Examples
 #'
-#' @description A random set of 10 nesting attempts from the NewstWatch database to use used in running R documentation examples.
+#' @description A random set of 10 nesting attempts from the NewstWatch database to use in running R documentation examples.
 #'
 #' @format A dataframe with 79 observations of 21 variables. See <https://doi.org/10.1002/ecy.4230> for details of variables.
 "exNWchecks"
